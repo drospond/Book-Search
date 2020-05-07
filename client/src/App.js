4fd235a92ecb41c22b81a85aa9054b503d74adf4
drospond <david.rospond@gmail.com>
2020-05-07 17:45:09 -0400

    setup react router

--- client/src/App.js
+++ client/src/App.js
@@ -1,10 +1,23 @@
 import React from "react";
 import "./App.css";
+import { BrowserRouter, Route, Switch } from "react-router-dom";
+import SavedBooks from "./containers/SavedBooks";
+import SearchBooks from "./containers/SearchBooks";
 
 function App() {
   return (
     <div className="App">
       <h1>Hello</h1>
+      <BrowserRouter>
+        <Switch>
+          <Route exact path="/(|saved)">
+            <SavedBooks/>
+          </Route>
+          <Route exact path="/search"> 
+            <SearchBooks/>
+          </Route>
+        </Switch>
+      </BrowserRouter>
     </div>
   );
 }
